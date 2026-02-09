f794f152fd5d27a345edc2e4c6ea559e30c3e9d7
ParryUser <waseemblue2007@gmail.com>
2026-02-09 17:37:33 +0300

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,10 @@ import 'package:flutterfirsttest/db/DataBaseHelper.dart';
 import 'package:flutterfirsttest/widgets/Labels.dart';
 import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await dotenv.load();
   print("main");
 
   final db = DataBaseHelper();
